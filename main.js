47bc92f93458850ad4e4895f788d68a6a141c690
conrad <yhg0337@naver.com>
2020-11-17 17:12:31 +0900

    ESLint-setting

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import App from "./App";
+import Vue from 'vue'
+import App from './App'
 
 new Vue({
-  el: "#app",
-  render: (createElement) => createElement(App),
-});
-//보통 h=>h(App) 으로 사용
+  el: '#app',
+  render: (createElement) => createElement(App)
+})
+// 보통 h=>h(App) 으로 사용
