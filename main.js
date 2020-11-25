a448d819f30a9db1c8e5adfd016e94ad83062d9a
conrad <yhg0337@naver.com>
2020-11-25 23:47:04 +0900

    vuex setting

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import store from "./store";
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  render: (createElement) => createElement(App)
-})
+  store,
+  render: (createElement) => createElement(App),
+});
 // 보통 h=>h(App) 으로 사용
---
+++ store/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import TodoApp from "to";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  // strict: true, // 배포가될때는 false 개발시에는 true
+  strict: process.env.NODE_ENV !== "production",
+  modules:{
+      todoApp
+  }
+  // Data
+  state: {},
+  // Computed
+  getters: {},
+  // Methods 실제값을 변경할때 (비동기 X)
+  mutations: {},
+  // Methods 비동기를 포함하는 일반로직
+  actions: {},
+});
